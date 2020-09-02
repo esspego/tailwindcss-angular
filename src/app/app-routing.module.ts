677da4a681f2930295d85a09b13678d4e172eccc
Espe <ego.egarco01@gmail.com>
2020-09-02 22:41:24 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StyleGuideComponent } from './pages/style-guide/style-guide.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent} ,
+  { path: 'contact', component: HomeComponent} ,
+  { path: 'about', component: HomeComponent} ,
+  { path: 'style-guide', component: StyleGuideComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StyleGuideComponent } from './pages/style-guide/style-guide.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StyleGuideComponent,
+    NavbarComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
